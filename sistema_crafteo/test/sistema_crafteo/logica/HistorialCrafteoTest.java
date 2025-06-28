84930301fab358c1f6d9416bfb413946d371c32d
Gonzalo Rodriguez <rodriguezgonzalo1204@gmail.com>
2025-06-28 17:31:50 -0300

    Add JSON loader and tests

--- sistema_crafteo/test/sistema_crafteo/logica/HistorialCrafteoTest.java
+++ sistema_crafteo/test/sistema_crafteo/logica/HistorialCrafteoTest.java
@@ -2,13 +2,22 @@ package sistema_crafteo.logica;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDateTime;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
+import sistema_crafteo.objeto.IngredienteBasico;
+import sistema_crafteo.objeto.Item;
+
 class HistorialCrafteoTest {
-
-	@Test
-	void test() {
-		assertTrue(true);
-	}
-
+    @Test
+    void crearRegistro_guardaDatosCorrectamente() {
+        Item madera = new IngredienteBasico("madera"," ");
+        LocalDateTime ahora = LocalDateTime.now();
+        HistorialCrafteo h = new HistorialCrafteo(madera, Map.of(madera,1), ahora);
+        assertEquals(madera, h.getItem());
+        assertTrue(h.getIngredientes().containsKey(madera));
+        assertEquals(ahora, h.getFecha());
+    }
 }
